c80c1b610161e62c82d2b68fb19901e0e807fe76
Yerkebulan Serikbayev <y.serikbayev@ssa.group>
2022-05-05 17:06:12 +0600

    Fixed small bug

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -1,18 +1,18 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect, useRef } from "react";
+import { View, Text, StyleSheet } from "react-native";
 
-import { colors } from '../utils/colors';
-import { fontSizes, spacing } from '../utils/sizes';
+import { colors } from "../utils/colors";
+import { fontSizes, spacing } from "../utils/sizes";
 
 const minsToMillis = (mins) => mins * 1000 * 60;
 
-const formatTime = (time) => time < 10 ? `0${time}` : time;
+const formatTime = (time) => (time < 10 ? `0${time}` : time);
 
 export const Countdown = ({
   minutes = 20,
   isPaused = true,
   onProgress,
-  onEnd
+  onEnd,
 }) => {
   const interval = useRef(null);
   const [millis, setMillis] = useState(0);
@@ -22,7 +22,7 @@ export const Countdown = ({
       if (interval.current) {
         clearInterval(interval.current);
       }
-      
+
       return;
     }
 
@@ -38,7 +38,7 @@ export const Countdown = ({
   useEffect(() => {
     onProgress(millis / minsToMillis(minutes));
 
-    if (millis === 0) {
+    if (millis === 0 && !isPaused) {
       onEnd();
     }
   }, [millis]);
@@ -54,22 +54,26 @@ export const Countdown = ({
       const timeLeft = time - 1000;
 
       return timeLeft;
-    })
-  }
+    });
+  };
 
   const mins = Math.floor(millis / 1000 / 60) % 60;
   const seconds = Math.floor(millis / 1000) % 60;
 
-  return <Text style={styles.text}>{formatTime(mins)}:{formatTime(seconds)}</Text>
-}
+  return (
+    <Text style={styles.text}>
+      {formatTime(mins)}:{formatTime(seconds)}
+    </Text>
+  );
+};
 
 const styles = StyleSheet.create({
   text: {
     fontSize: fontSizes.xxxl,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     color: colors.white,
-    textAlign: 'center',
+    textAlign: "center",
     padding: spacing.lg,
-    backgroundColor: 'rgba(94, 132, 226, 0.3)'
-  }
-})
+    backgroundColor: "rgba(94, 132, 226, 0.3)",
+  },
+});
--- src/features/timer/Timer.js
+++ src/features/timer/Timer.js
@@ -1,22 +1,18 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, Vibration } from 'react-native';
-import { ProgressBar } from 'react-native-paper';
-import { useKeepAwake } from 'expo-keep-awake';
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Vibration } from "react-native";
+import { ProgressBar } from "react-native-paper";
+import { useKeepAwake } from "expo-keep-awake";
 
-import { colors } from '../../utils/colors';
-import { spacing } from '../../utils/sizes';
-import { Countdown } from '../../components/Countdown';
-import { RoundedButton } from '../../components/RoundedButton';
+import { colors } from "../../utils/colors";
+import { spacing } from "../../utils/sizes";
+import { Countdown } from "../../components/Countdown";
+import { RoundedButton } from "../../components/RoundedButton";
 
-import { Timing } from './Timing';
+import { Timing } from "./Timing";
 
-const DEFAULT_TIME = 0.2;
+const DEFAULT_TIME = 1;
 
-export const Timer = ({
-  focusSubject,
-  onTimerEnd,
-  clearSubject
-}) => {
+export const Timer = ({ focusSubject, onTimerEnd, clearSubject }) => {
   useKeepAwake();
 
   const [minutes, setMinutes] = useState(DEFAULT_TIME);
@@ -29,7 +25,7 @@ export const Timer = ({
     setProgress(1);
 
     setIsStarted(false);
-  }
+  };
 
   const handleTimeEnd = () => {
     vibrate();
@@ -41,72 +37,81 @@ export const Timer = ({
     setIsStarted(false);
 
     onTimerEnd();
-  }
+  };
 
   const vibrate = () => {
     const interval = setInterval(() => Vibration.vibrate(), 1000);
     setTimeout(() => clearInterval(interval), 10000);
-  }
+  };
 
-  return (<View style={styles.container}>
-    <View style={styles.countdown}>
-      <Countdown minutes={minutes} isPaused={!isStarted} onProgress={setProgress} onEnd={handleTimeEnd} />
+  return (
+    <View style={styles.container}>
+      <View style={styles.countdown}>
+        <Countdown
+          minutes={minutes}
+          isPaused={!isStarted}
+          onProgress={setProgress}
+          onEnd={handleTimeEnd}
+        />
+      </View>
+      <View style={{ paddingTop: spacing.xxl }}>
+        <Text style={styles.title}>Focus on:</Text>
+        <Text style={styles.task}>{focusSubject}</Text>
+      </View>
+      <View style={{ paddingTop: spacing.sm }}>
+        <ProgressBar
+          progress={progress}
+          color="#5E84E2"
+          style={styles.progress}
+        />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Timing onChangeTime={handleChangeTime} />
+      </View>
+      <View style={styles.buttonContainer}>
+        {isStarted ? (
+          <RoundedButton title="pause" onPress={() => setIsStarted(false)} />
+        ) : (
+          <RoundedButton title="start" onPress={() => setIsStarted(true)} />
+        )}
+      </View>
+      <View style={styles.clearSubject}>
+        <RoundedButton title="-" size={50} onPress={clearSubject} />
+      </View>
     </View>
-    <View style={{ paddingTop: spacing.xxl }}>
-      <Text style={styles.title}>Focus on:</Text>
-      <Text style={styles.task}>{focusSubject}</Text>
-    </View>
-    <View style={{ paddingTop: spacing.sm }}>
-      <ProgressBar 
-        progress={progress}
-        color="#5E84E2"
-        style={styles.progress}
-      />
-    </View>
-    <View style={styles.buttonContainer}>
-      <Timing onChangeTime={handleChangeTime} />
-    </View>
-    <View style={styles.buttonContainer}>
-      {isStarted 
-        ? <RoundedButton title="pause" onPress={() => setIsStarted(false)} /> 
-        : <RoundedButton title="start" onPress={() => setIsStarted(true)} />}
-    </View>
-    <View style={styles.clearSubject}>
-      <RoundedButton title="-" size={50} onPress={clearSubject} /> 
-    </View>
-  </View>)
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
   },
   title: {
     color: colors.white,
-    textAlign: 'center'
+    textAlign: "center",
   },
   task: {
     color: colors.white,
-    textAlign: 'center',
-    fontWeight: 'bold'
+    textAlign: "center",
+    fontWeight: "bold",
   },
   countdown: {
     flex: 0.5,
-    justifyContent: 'center',
-    alignItems: 'center'
+    justifyContent: "center",
+    alignItems: "center",
   },
   progress: {
-    height: 10
+    height: 10,
   },
   buttonContainer: {
     flex: 0.3,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
     padding: spacing.md,
   },
   clearSubject: {
     paddingBottom: spacing.lg,
-    paddingLeft: spacing.lg
-  }
-})
+    paddingLeft: spacing.lg,
+  },
+});
